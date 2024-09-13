670ccd926ff78daa19889a5b46964137e31e4d66
Dell E5440 i5 <nelson.ade.quadri@gmail.com>
2024-09-13 12:34:07 +0100

    I added icons to the side bar, but intend to make it better though

--- src/components/pages/SideBar/SideBar.jsx
+++ src/components/pages/SideBar/SideBar.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import "./SideBar.css";
 import logo from "../../../assets/trakFundzLogoReal.svg";
 import { NavLink, useNavigate } from "react-router-dom";
+import { AiFillHome } from "react-icons/ai";
+import { GiTakeMyMoney } from "react-icons/gi";
+import { GiPiggyBank } from "react-icons/gi";
+import { GiReceiveMoney } from "react-icons/gi";
+import { FcBullish } from "react-icons/fc";
+import { IoSettingsSharp } from "react-icons/io5";
+import { TbHelpTriangle } from "react-icons/tb";
+import { AiOutlinePoweroff } from "react-icons/ai";
 
 const SideBar = () => {
-    const Nav = useNavigate()
+  const Nav = useNavigate();
   return (
     <div className="sideBarHolder">
       <div className="sideBarImg">
@@ -16,10 +24,16 @@ const SideBar = () => {
           <NavLink
             className={({ isActive }) =>
               isActive ? "HeaderActive" : "HeaderNotActive"
-            } 
+            }
             to={"/dashboard/home"}
           >
-            <h5 className="sideBarH5"> Dashboard </h5>
+            <h5 className="sideBarH5">
+              <AiFillHome
+                className="iconHomeDash"
+                style={{ fontSize: "20px" }}
+              />
+              Dashboard{" "}
+            </h5>
           </NavLink>
 
           <NavLink
@@ -28,7 +42,13 @@ const SideBar = () => {
             }
             to={"/dashboard/expense-track"}
           >
-            <h5 className="sideBarH5"> Expense Tracker </h5>
+            <h5 className="sideBarH5">
+              <GiTakeMyMoney
+                className="iconExpense"
+                style={{ fontSize: "23px" }}
+              />
+              Expense Tracker{" "}
+            </h5>
           </NavLink>
 
           <NavLink
@@ -37,7 +57,13 @@ const SideBar = () => {
             }
             to={"/dashboard/budget-planner"}
           >
-            <h5 className="sideBarH5"> Budget Planner </h5>
+            <h5 className="sideBarH5">
+              <GiPiggyBank
+                className="iconPiggyBudget"
+                style={{ fontSize: "23px" }}
+              />
+              Budget Planner{" "}
+            </h5>
           </NavLink>
 
           <NavLink
@@ -46,7 +72,13 @@ const SideBar = () => {
             }
             to={"/dashboard/debt-manager"}
           >
-            <h5 className="sideBarH5"> Debt Manager </h5>
+            <h5 className="sideBarH5">
+              <GiReceiveMoney
+                className="iconDebt"
+                style={{ fontSize: "23px", color: "red" }}
+              />
+              Debt Manager{" "}
+            </h5>
           </NavLink>
 
           <NavLink
@@ -55,7 +87,10 @@ const SideBar = () => {
             }
             to={"/dashboard/report"}
           >
-            <h5 className="sideBarH5"> Report Insights </h5>
+            <h5 className="sideBarH5">
+              <FcBullish className="iconRepo" style={{ fontSize: "23px", color: "red" }} />
+              Report Insights{" "}
+            </h5>
           </NavLink>
 
           <NavLink
@@ -64,10 +99,15 @@ const SideBar = () => {
             }
             to={"/dashboard/settings"}
           >
-            <h5 className="sideBarH5"> Settings </h5>
+            <h5 className="sideBarH5">
+              <IoSettingsSharp
+                className="iconSettings"
+                style={{ fontSize: "23px" }}
+              />
+              Settings{" "}
+            </h5>
           </NavLink>
         </div>
-
 
         <div className="downFonts">
           <NavLink
@@ -76,7 +116,10 @@ const SideBar = () => {
             }
             to={"/dashboard/help"}
           >
-            <h5 className="sideBarH5"> Help </h5>
+            <h5 className="sideBarH5">
+              <TbHelpTriangle className="icon" style={{ fontSize: "23px" }} />
+              Help{" "}
+            </h5>
           </NavLink>
 
           <NavLink
@@ -85,7 +128,10 @@ const SideBar = () => {
             }
             to={"/"}
           >
-            <h5 className="sideBarH5"> Log Out</h5>
+            <h5 className="sideBarH5">
+              <AiOutlinePoweroff  style={{fontSize: "23px"}}/>
+              Log Out
+            </h5>
           </NavLink>
         </div>
       </div>
