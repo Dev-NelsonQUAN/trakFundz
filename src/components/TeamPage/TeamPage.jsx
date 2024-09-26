69b26f0a66744c3caba6da04e93acc0b08b2b244
Dell E5440 i5 <nelson.ade.quadri@gmail.com>
2024-09-26 01:19:18 +0100

    Last push for TrakFundz

--- src/components/TeamPage/TeamPage.jsx
+++ src/components/TeamPage/TeamPage.jsx
@@ -1,43 +1,74 @@
-import React from 'react';
-import './TeamPage.css';
-import Oriade from '../../assets/Oriade.svg'; 
-import Newness from '../../assets/Newness.svg';
-import Emeka from '../../assets/Emeka.svg';
-import Shola from '../../assets/Shola.svg';
+import React from "react";
+import "./TeamPage.css";
+import Oriade from "../../assets/Oriade.svg";
+import Newness from "../../assets/Newness.svg";
+import Emeka from "../../assets/Emeka.svg";
+import Shola from "../../assets/Shola.svg";
+import Nelz from "../../assets/nelzAde.svg";
+import aAbayo from "../../assets/aAba.svg";
 
 // Sample team data
 const teamMembers = [
-  { id: 1, name: 'Adeola Oriade Olaitan', stack: 'Product Designer', image: Oriade },
-  { id: 2, name: 'Emeka Chigbata', stack: 'Back End Developer', image:  Emeka},
-  { id: 3, name: 'Ogedengbe Oluwashola', stack: 'Back End Developer', image: Shola },
-  { id: 4, name: 'Quadri Adegbenga Nelson', stack: 'Front End Developer', image: Oriade },
-  { id: 5, name: 'Ibrahim Memunat Bolanle', stack: 'Front End Developer', image: Newness },
-  { id: 6, name: 'Abari Abayomi', stack: 'Front End Developer', image: Oriade }
+  {
+    id: 1,
+    name: "Adeola Oriade Olaitan",
+    stack: "Product Designer",
+    image: Oriade,
+  },
+  { id: 2, name: "Emeka Chigbata", 
+    stack: "Back End Developer", 
+    image: Emeka 
+  },
+  {
+    id: 3,
+    name: "Ogedengbe Oluwashola",
+    stack: "Back End Developer",
+    image: Shola,
+  },
+  {
+    id: 4,
+    name: "Quadri Adegbenga Nelson",
+    stack: "Front End Developer",
+    image: Nelz,
+  },
+  {
+    id: 5,
+    name: "Ibrahim Memunat Bolanle",
+    stack: "Front End Developer",
+    image: Newness,
+  },
+  { id: 6, name: "Abari Abayomi", 
+    stack: "Front End Developer", 
+    image: aAbayo 
+  },
 ];
 
 const TeamPage = () => {
   return (
     <>
-    <div className="teamContainer">
+      <div className="teamContainer">
         <h1>Meet the team</h1>
-      {teamMembers.map(member => (
-        <div key={member.id} className="teamBox">
-          <img src={member.image} alt={member.name} className="teamImage" />
-          <h3 className="teamName">{member.name}</h3>
-          <p className="teamStack">{member.stack}</p>
+        {teamMembers.map((member) => (
+          <div key={member.id} className="teamBox">
+            <img src={member.image} alt={member.name} className="teamImage" />
+            <h3 className="teamName">{member.name}</h3>
+            <p className="teamStack">{member.stack}</p>
+          </div>
+        ))}
+      </div>
+      <div className="teamText">
+        <div className="teamWords">
+          <p>
+            We're excited about the future and committed to continuously
+            improving Trakfundz to better serve our users. Your feedback and
+            support are invaluable as we strive to make financial management
+            easier and more effective for everyone. Thank you for choosing
+            Trakfundz. Together, we can achieve financial clarity and success.
+          </p>
+          <span className="Dteam">The Trakfundz Team</span>
         </div>
-      ))}
-    </div>
-    <div className='teamText'>
-    <div className='teamWords'>
-        <p>We’re excited about the future and committed to continuously improving Trakfundz to better serve our users. Your feedback 
-and support are invaluable as we strive to make financial management easier and more effective for everyone.
-
-Thank you for choosing Trakfundz. Together, we can achieve financial clarity and success.</p>
-       <span className='Dteam'>The Trakfundz Team</span>
-    </div>
-  </div>
-  </>
+      </div>
+    </>
   );
 };
 
